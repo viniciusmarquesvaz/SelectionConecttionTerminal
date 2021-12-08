e7d9b475799b1599249cd9226918f877d80f9202
viniciusmarquesvaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2021-12-08 09:48:22 -0300

    comit

--- Terminal.sh
+++ Terminal.sh
@@ -1,87 +1,31 @@
 #!/bin/bash
-ITEM_SELECIONADO=$(zenity  --list  --text "Selecione o Terminal que deseja entrar" \
-    --radiolist \
-    --column "Marcar" \
-    --column "Ambiente Terminal" \
-    FALSE Financeiro TRUE Rh FALSE Compras FALSE Ciex FALSE Dp False Logística FALSE );
-if [ ITEM_SELECIONADO  == "TRUE Financeiro" ]; then    
-xfreerdp /v:ts.compras\
-/multimon \
-/u:$(zenity \
---entry \
---title="Usuário" \
---text="Digite o usuário") \
-/p:$(zenity \
---entry \
---title="Senha" \
---text="Digite a senha:" \
---hide-text) \
-/d:farid \
-/f \
-/cert-ignore
-+clipboard
-elif [ ITEM_SELECIONADO  == "Rh" ]; then    
-xfreerdp /v:ts.Rh\
-/multimon \
-/u:$(zenity \
---entry \
---title="Usuário" \
---text="Digite o usuário") \
-/p:$(zenity \
---entry \
---title="Senha" \
---text="Digite a senha:" \
---hide-text) \
-/d:farid \
-/f \
-/cert-ignore
-+clipboard
-elif [ ITEM_SELECIONADO  == "Logística" ]; then    
-xfreerdp /v:ts.Logística\
-/multimon \
-/u:$(zenity \
---entry \
---title="Usuário" \
---text="Digite o usuário") \
-/p:$(zenity \
---entry \
---title="Senha" \
---text="Digite a senha:" \
---hide-text) \
-/d:farid \
-/f \
-/cert-ignore
-+clipboard
-elif [ ITEM_SELECIONADO  == "Ciex" ]; then    
-xfreerdp /v:ts.Ciex\
-/multimon \
-/u:$(zenity \
---entry \
---title="Usuário" \
---text="Digite o usuário") \
-/p:$(zenity \
---entry \
---title="Senha" \
---text="Digite a senha:" \
---hide-text) \
-/d:farid \
-/f \
-/cert-ignore
-+clipboard
-elif [ ITEM_SELECIONADO  == "Dp" ]; then    
-xfreerdp /v:ts.Dp\
-/multimon \
-/u:$(zenity \
---entry \
---title="Usuário" \
---text="Digite o usuário") \
-/p:$(zenity \
---entry \
---title="Senha" \
---text="Digite a senha:" \
---hide-text) \
-/d:farid \
-/f \
-/cert-ignore
-+clipboard
 
+USUARIO=$(zenity --entry --title="Usuário" --text="Digite o usuário")
+PASS=$(zenity --entry --title="Senha" --text="Digite a senha:" --hide-text)
+GROUPS=$(ldapsearch -x -H "ldap://192.168.0.18" -b "dc=intranet,dc=farid" -D "cn=ntservicos,ou=Servicos,dc=intranet,dc=farid" -w "e9.vf4?f76" "sAMAccountName=${USUARIO}" memberof | sed -n "/^memberOf:/p")
+SERVER="ts.intranet.farid"
+
+echo $GROUPS
+case $GROUPS in
+	*"RDS-LOGISTICA"*)
+		SERVER="ts-logistica.intranet.farid"
+		;;
+	*"RDS-MAISCOR"*)
+		SERVER="ts-maiscor.intranet.farid"
+		;;
+	*"RDS-FINANCEIRO"*)
+		SERVER="ts-financeiro.intranet.farid"
+		;;
+	*"RDS-CONTABILIDADE"*)
+		SERVER="ts-contabilidade.intranet.farid"
+		;;
+	*"RDS-COMPRAS"*)
+		SERVER="ts-compras.intranet.farid"
+		;;
+	*"RDS-RH"*)
+		SERVER="ts-rh.intranet.farid"
+		;;
+esac
+
+echo $SERVER
+xfreerdp /v:$SERVER /multimon /u:$USUARIO /p:$PASS /d:farid /f /cert-ignore +clipboard
